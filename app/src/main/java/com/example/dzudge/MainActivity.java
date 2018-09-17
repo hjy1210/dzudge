e4cf15ac341abf6f8668a996f2ebc3460a1796ff
Hon-Jang Yang <hjyanghj@gmail.com>
2018-09-17 10:52:36 +0800

    add appbar with menu 1. modify MainActivity as a main shell with appbar and menu 2. create EditMessageActivity to replace original MainActivity, 3. use menuitem click to show up EditMessageActivity

--- app/src/main/java/com/example/dzudge/MainActivity.java
+++ app/src/main/java/com/example/dzudge/MainActivity.java
@@ -3,21 +3,49 @@ package com.example.dzudge;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String EXTRA_MESSAGE = "com.example.dzudge.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void sendMessage(View view){
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
     }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        TextView textView;
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                //textView = (TextView) findViewById(R.id.textView);
+                //textView.setText("Action_Settings");
+                return true;
+            case R.id.action_favorite:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                //textView = (TextView) findViewById(R.id.textView);
+                //textView.setText("楊宏章");
+                return true;
+            case R.id.action_testing:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                //textView = (TextView) findViewById(R.id.textView);
+                //textView.setText("楊宏章");
+                Intent intent=new Intent(this,EditMessageActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
