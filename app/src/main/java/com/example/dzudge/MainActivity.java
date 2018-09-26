93773464059fb1a168fb2f8f4839b03ad415a813
Hon-Jang Yang <hjyanghj@gmail.com>
2018-09-26 17:41:16 +0800

    live track curPos

--- app/src/main/java/com/example/dzudge/MainActivity.java
+++ app/src/main/java/com/example/dzudge/MainActivity.java
@@ -1,11 +1,19 @@
 package com.example.dzudge;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -24,6 +32,7 @@ import org.mapsforge.map.android.view.MapView;
 import org.mapsforge.map.datastore.MapDataStore;
 import org.mapsforge.map.layer.Layer;
 import org.mapsforge.map.layer.cache.TileCache;
+import org.mapsforge.map.layer.overlay.Circle;
 import org.mapsforge.map.layer.overlay.Marker;
 import org.mapsforge.map.layer.overlay.Polyline;
 import org.mapsforge.map.layer.renderer.TileRendererLayer;
@@ -42,7 +51,7 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
     private static final int READ_REQUEST_GPX = 42;
 
     private MapDataStore mapDataStore;
@@ -54,12 +63,36 @@ public class MainActivity extends AppCompatActivity {
     private byte zoomLevel;
     private TileCache tileCache;
     private ArrayList<Layer> trackLayers;
+    private Layer lastPos;
+
+    private LocationManager locationManager;
+    private String provider;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        // Define the criteria how to select the locatioin provider -> use
+        // default
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, false);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        //Location location = locationManager.getLastKnownLocation(provider);
+
         //setContentView(R.layout.activity_main);
         map = "GTs/map/taiwan_ML.map";
-        mapFile=new File(Environment.getExternalStorageDirectory(),map);
+        mapFile = new File(Environment.getExternalStorageDirectory(), map);
         zoomLevel = 13;
         //mapView=(MapView) findViewById(R.id.map);
         /*
@@ -107,34 +140,52 @@ public class MainActivity extends AppCompatActivity {
              */
             e.printStackTrace();
         }
-        Toast.makeText(this,"onCreate",Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "onCreate", Toast.LENGTH_LONG).show();
 
     }
+
     @Override
     protected void onStart() {
         super.onStart();
-        Toast.makeText(this,"onStart",Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "onStart", Toast.LENGTH_LONG).show();
     }
+
     @Override
     protected void onRestart() {
         super.onRestart();
-        Toast.makeText(this,"onRestart",Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "onRestart", Toast.LENGTH_LONG).show();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Toast.makeText(this,"onResume",Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "onResume", Toast.LENGTH_LONG).show();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        if (mapDataStore!=null)
+            locationManager.requestLocationUpdates(provider, 300, 5, this);
     }
+
     @Override
     protected void onPause() {
         super.onPause();
-        Toast.makeText(this,"onPause",Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "onPause", Toast.LENGTH_LONG).show();
+        locationManager.removeUpdates(this);
     }
+
     @Override
     protected void onStop() {
         super.onStop();
-        Toast.makeText(this,"onStop",Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "onStop", Toast.LENGTH_LONG).show();
     }
 
     private void setMapCenter(MapDataStore mapDataStore, byte zoomLevel) {
@@ -147,8 +198,33 @@ public class MainActivity extends AppCompatActivity {
         double minLat = mapDataStore.boundingBox().minLatitude;
         double maxLong = mapDataStore.boundingBox().maxLongitude;
         double minLong = mapDataStore.boundingBox().minLongitude;
+        LatLong center = new LatLong((maxLat + minLat) / 2, (maxLong + minLong) / 2);
         mapView.setCenter(new LatLong((maxLat + minLat) / 2, (maxLong + minLong) / 2));
         mapView.setZoomLevel(zoomLevel);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(provider, 300, 5, this);
+        /*Circle circle = new Circle(center, 50, Utils.createPaint(
+                AndroidGraphicFactory.INSTANCE.createColor(Color.BLUE), 0,
+                Style.FILL), null);
+        lastPos=circle;
+        mapView.getLayerManager().getLayers().add(circle);
+        Toast.makeText(this,"wait",Toast.LENGTH_SHORT).show();
+        mapView.getLayerManager().getLayers().remove(lastPos);
+        center=new LatLong((maxLat + minLat) / 2+0.01, (maxLong + minLong) / 2+0.01);
+        circle = new Circle(center, 50, Utils.createPaint(
+                AndroidGraphicFactory.INSTANCE.createColor(Color.BLUE), 0,
+                Style.FILL), null);
+        mapView.getLayerManager().getLayers().add(circle);*/
+
     }
 
     @Override
@@ -331,5 +407,41 @@ public class MainActivity extends AppCompatActivity {
             inputStream.close();
         }
         return null;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        double lat = (location.getLatitude());
+        double lng = (location.getLongitude());
+        LatLong center=new LatLong(lat,lng);
+        if (lastPos!=null){
+            mapView.getLayerManager().getLayers().remove(lastPos);
+        }
+        //lastPos = new Circle(center, 5, Utils.createPaint(
+        //            AndroidGraphicFactory.INSTANCE.createColor(Color.BLUE), 0,
+        //            Style.FILL), null);
+        lastPos = Utils.createMarker(this,
+                R.drawable.marker_green,center);
+        mapView.getLayerManager().getLayers().add(lastPos);
+        mapView.setCenter(center);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Toast.makeText(this, "Enabled new provider " + provider,
+                Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(this, "Disabled provider " + provider,
+                Toast.LENGTH_SHORT).show();
     }
 }
